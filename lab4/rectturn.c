9c95333d9796440f72c53b6d19413d897393c844
gagask <42906191+gagask@users.noreply.github.com>
2019-04-25 09:56:28 +0300

    Update rectturn.c

--- lab4/rectturn.c
+++ lab4/rectturn.c
@@ -14,29 +14,29 @@ void initTurn(turn* tr){
     tr->last=-1;
 }
 
+int isEmpty(turn *tr){
+    if (tr->first==tr->last) return 1;
+    else return 0;
+}
+
 char pop(turn *tr){
-    tr->first=(tr->first+1)%NEW;
-    return tr->arr[tr->first];
+        if (isEmpty(tr)) return '.';
+        tr->first=(tr->first+1)%NEW;
+        return tr->arr[tr->first];
 }
 
 void push(char b,turn *tr){
     tr->last=(tr->last+1)%NEW;
     tr->arr[tr->last]=b;
-    //tr->last=(tr->last+1)%NEW;
 }
 
-int isEmpty(turn *tr){
-    if (tr->last==-1) return 1;
-    if (tr->first==tr->last) return 1;
-    else return 0;
-}
-
-int count(turn *tr){ 
+int count(turn *tr){
     return (NEW+tr->last-tr->first)%NEW;
 }
 
 char top(turn* tr){
-    return tr->arr[(tr->first+1)%NEW];
+        if (isEmpty(tr)) return '.';
+        return tr->arr[(tr->first+1)%NEW];
 }
 
 int main(){
@@ -49,7 +49,7 @@ int main(){
     push('c',&tr);
     push('d',&tr);
     push('e',&tr);
-    printf("top is %c\n",top(&tr));
+    printf("top is %c\ncount:%d\n",top(&tr),count(&tr));
     printf("%c\n",pop(&tr));
     push('f',&tr);
     printf("%c\n",pop(&tr));
